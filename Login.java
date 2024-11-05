558795c6f056f0bf008688cb4e273a402b8fcbe4
HarshKorde5 <harshkorde05@gmail.com>
2024-11-06 00:43:29 +0530

    icon-login

--- Login.java
+++ Login.java
@@ -3,16 +3,18 @@ import javax.swing.*;
 import java.awt.*;
 // import javax.swing.border.LineBorder;
 
-class loginWindow extends FocusAdapter
+class loginWindow extends FocusAdapter implements ActionListener
 {
     public JFrame frame;
-    public JLabel headerLabel;
-    public JTextField usernameField,passwordField;
+    public JLabel headerLabel,startSharingLabel;
+    public JTextField usernameField;
     public JButton loginButton,clearButton;
 
     public void displayWindow()
     {
         frame = new JFrame("File Sharing Portal");
+        
+        frame.setIconImage(new ImageIcon("file.jpg").getImage());
         frame.setSize(600,600);
         frame.setLayout(null);
         frame.setLocationRelativeTo(null);
@@ -21,29 +23,30 @@ class loginWindow extends FocusAdapter
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         headerLabel = new JLabel("Log in");
-        headerLabel.setBounds(220,70,200,100);
-        // header.setPreferredSize(new Dimension(200,200));
+        headerLabel.setBounds(220,70,200,80);
         headerLabel.setFont(new Font("Calibri",Font.BOLD,50));
         frame.add(headerLabel);
 
-        usernameField = new JTextField("Enter username",50);
-        passwordField = new JTextField("Enter password",50);
+        startSharingLabel = new JLabel("Start sharing as : ");
+        startSharingLabel.setBounds(160,200,350,50);
+        startSharingLabel.setFont(new Font("Calibri",Font.BOLD,30));
+        frame.add(startSharingLabel);
 
-        usernameField.setBounds(100,200,400,50);
-        passwordField.setBounds(100,300,400,50);
-
+        usernameField = new JTextField("Enter a username",50);
+        usernameField.setBounds(100,270,400,50);
         usernameField.addFocusListener(this);
-        passwordField.addFocusListener(this);
 
         // usernameField.setBorder(new LineBorder(Color.black,1,true));
         frame.add(usernameField);
-        frame.add(passwordField);
 
         loginButton = new JButton("Login");
         clearButton = new JButton("Clear");
 
         loginButton.setBounds(350,380,100,50);
         clearButton.setBounds(150,380,100,50);
+
+        loginButton.addActionListener(this);
+        clearButton.addActionListener(this);
 
         frame.add(loginButton);
         frame.add(clearButton);
@@ -55,9 +58,29 @@ class loginWindow extends FocusAdapter
         {
             usernameField.setText("");
         }
-        if(f.getSource() == passwordField)
+    }
+
+    public void actionPerformed(ActionEvent e)
+    {
+        if(e.getSource() == clearButton)
         {
-            passwordField.setText("");
+            usernameField.setText("");
+        }
+        if(e.getSource() == loginButton)
+        {
+            String username = usernameField.getText();
+            if(!username.equals(""))
+            {
+                System.out.println("Welcome "+username+"\nPlease select to send or recieve files");
+            }
+            else
+            {
+                //display msg dialog box
+                JOptionPane.showMessageDialog(usernameField, "Please enter a valid username", 
+                                          "Alert", 
+                                          JOptionPane.INFORMATION_MESSAGE);
+
+            }
         }
     }
 }
