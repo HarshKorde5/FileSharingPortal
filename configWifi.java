13d7ceb4238fa5f04cd2ad2d3752efe344c7b89d
HarshKorde5 <harshkorde05@gmail.com>
2024-11-05 23:48:17 +0530

    wifiConfig

--- configWifi.java
+++ configWifi.java
@@ -3,9 +3,12 @@ import java.util.*;
 
 class configWifi
 {
-    public static void main(String arg[]) throws Exception
+    public static boolean connectRoom(String ssid) throws Exception
     {
-        String turnOn[] = {"nmcli", "device", "wifi", "list"};
+        boolean connected = true;
+        String password = "fileshare";
+
+        String[] turnOn = {"nmcli","device","wifi","connect",ssid,"password",password};
 
         ProcessBuilder pb = new ProcessBuilder(turnOn);
         Process p = pb.start();
@@ -13,12 +16,65 @@ class configWifi
         BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
         String line;
 
-        while((line = reader.readLine()) != null)
+        while((line = reader.readLine())!= null)
         {
+            if(line.startsWith("Error"))
+            {
+                connected = false;
+                break;
+            }
             System.out.println(line);
         }
 
+        return connected;
+    }
+
+    public static void main(String arg[]) throws Exception
+    {
+        String getList[] = {"nmcli","-f", "SSID", "device", "wifi", "list"};  //17length
+        // String turnOn[] = {"nmcli","device", "wifi", "list"};    //94length
+
+        boolean connected = false;
+        String SSID = "";
+        boolean roomFound = false;
+
+        ProcessBuilder pb = new ProcessBuilder(getList);
+        Process p = pb.start();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        String line;
+
+        while((line = reader.readLine()) != null)
+        {
+            // System.out.println(line.length());
+            line = line.trim();
+
+            if(line.endsWith("room"))
+            {
+                System.out.println(line);
+                roomFound = true;
+                SSID = line;
+                System.out.println("SSID :: "+SSID);
+            }
+        }
+
         p.waitFor();
-        
+        if(!roomFound)
+        {
+          System.out.println("No room found! Try again");
+        }
+        else
+        {
+            connected = connectRoom(SSID);
+
+            if(connected)
+            {
+                System.out.println("Connection successful to "+SSID+", ready to share!");
+            }
+            else
+            {
+                System.out.println("Connection to the room "+SSID+" failed, try again!");
+            }
+        }
     }
 }
