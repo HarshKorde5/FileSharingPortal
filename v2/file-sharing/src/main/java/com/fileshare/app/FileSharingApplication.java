a5b6cd995eedfeede983e94b6e8c0cff57443978
HarshKorde5 <harshkorde05@gmail.com>
2026-06-12 14:19:06 +0530

    Project setup with maven

---
+++ v2/file-sharing/src/main/java/com/fileshare/app/FileSharingApplication.java
@@ -0,0 +1,9 @@
+package com.fileshare;
+
+public class FileSharingApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "File Sharing Portal - V2!" );
+    }
+}
